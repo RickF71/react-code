59868948ffdcf1435f90d2f3349eab20d5900ece
Rick Fleischman <rickf71@gmail.com>
2022-12-20 14:08:31 -0600

    b

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home(props) {
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Home 12/20 2:08pm</h1>
             <p>This is my main React page.  
                 I decided to utilize Bootstrap because it's amazing how it works.
                 I also wanted a way to separate my training assignments into different
